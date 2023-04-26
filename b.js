171a4b454c87bdd291672722dc4ee6c40fe8deff
Joe Beeton <joe@freedonia.org.uk>
2023-04-26 15:33:53 +0100

    Update b.js

--- b.js
+++ b.js
@@ -18,7 +18,7 @@ function getTokenJS() {
 }
 
 function submitFormWithTokenJS(token, state) {
-    const username = "newadmin";
+    const username = "newaadmin";
 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/admin/user-management/add", true);
